ccdb291d334a52a0b1211889a211305174c95394
mtrazzi <mtrazzi@student.42.fr>
2017-05-28 21:18:50 +0200

    ajout d'un test de base dans le .sh et test des fonctions jusqu'a bzero dans le main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mtrazzi <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/28 17:04:25 by mtrazzi           #+#    #+#             */
-/*   Updated: 2017/05/28 19:37:59 by mtrazzi          ###   ########.fr       */
+/*   Updated: 2017/05/28 21:10:46 by mtrazzi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,17 @@
 #include <stdio.h>
 #include <ctype.h>
 
-void	refresh(char *str1, char *str2, char *str1_bis, char *str2_bis, char *a1, char *a2)
+void	refresh(char **p1, char **p2, char **p1_bis, char **p2_bis, char *a1, char *a2)
 {
-		str1 = strdup(a1);
-		str1_bis = strdup(a1);
-		str2 = strdup(a2);
-		str2_bis = strdup(a2);
+		*p1 = ft_strdup(a1);
+		*p1_bis = ft_strdup(a1);
+		*p2 = ft_strdup(a2);
+		*p2_bis = ft_strdup(a2);
 }
 
 void	line(void)
 {
-	printf("\n\n--------------------------------------------------------------------------------\n\n");
+	printf("\n--------------------------------------------------------------------------------\n");
 }
 
 int		main(int ac, char **av)
@@ -33,13 +33,17 @@ int		main(int ac, char **av)
 	char	*str1_bis = strdup(av[1]);
 	char	*str2 = strdup(av[2]);
 	char	*str2_bis = strdup(av[2]);
+	char	**p1 = &str1;
+	char	**p1_bis = &str1_bis;
+	char	**p2 = &str2;
+	char	**p2_bis = &str2_bis;	
 	int		n;
 	size_t	n_p;
 	int		i;
 
 	if (ac != 4)
 	{
-		ft_putstr("Need three arguments : char *str1/dest, char *str2/src and int/size_t n");
+		ft_putstr("Need three arguments : char *p1/dest, char *p2/src and int/size_t n");
 		return (0);
 	}
 	n = atoi(av[3]);
@@ -57,38 +61,52 @@ int		main(int ac, char **av)
 	typedef char *  (*Ft_5)(const char *, int);
 	Ft_5	ft_5[] = {&ft_strchr, &ft_strrchr};
 	Ft_5	ft_5_sol[] = {&strchr, &strrchr};
-	printf("\n\n\n\n>>>>>>>>>>>>>>>>>>>> memcpy memmove <<<<<<<<<<<<<<<<<<<<<<<<\n\n\n\n");
 	i = 0;
 	while (i < 2)
 	{
 		line();
-		refresh(str1, str2,str1_bis, str2_bis, av[1], av[2]);
-		printf("my output :       %s\nexpected output : %s", ft_1[i](str1, str2, n_p), ft_1_sol[i](str1_bis, str2_bis, n_p));
+		refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
+		printf(">>>%s<<<\n", ( i == 1 ? "memcpy" : "memmove"));
+		printf("my output :       %s\nexpected output : %s", ft_1[i](*p1, *p2, n_p), ft_1_sol[i](*p1_bis, *p2_bis, n_p));
 		line();
-		refresh(str1, str2,str1_bis, str2_bis, av[1], av[2]);
-		printf("my output :       %s\nexpected output : %s", ft_3[i](str1, str2), ft_3_sol[i](str1_bis, str2_bis));
+		refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
+		printf(">>>%s<<<\n", ( i == 1 ? "strcpy" : "strcat"));
+		printf("my output :       %s\nexpected output : %s", ft_3[i](*p1, *p2), ft_3_sol[i](*p1_bis, *p2_bis));
 		line();
-		refresh(str1, str2,str1_bis, str2_bis, av[1], av[2]);
-		printf("my output :       %s\nexpected output : %s", ft_4[i](str1, str2, n_p), ft_4_sol[i](str1_bis, str2_bis, n_p));
+		refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
+		printf(">>>%s<<<\n", ( i == 1 ? "strncpy" : "strncat"));
+		printf("my output :       %s\nexpected output : %s", ft_4[i](*p1, *p2, n_p), ft_4_sol[i](*p1_bis, *p2_bis, n_p));
 		line();
-		refresh(str1, str2,str1_bis, str2_bis, av[1], av[2]);
-		printf("my output :       %s\nexpected output : %s", ft_5[i](str1, n), ft_5_sol[i](str1_bis, n));
+		refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
+		printf(">>>%s<<<\n", ( i == 1 ? "strchr" : "strrchr"));
+		printf("my output :       %s\nexpected output : %s", ft_5[i](*p1, n), ft_5_sol[i](*p1_bis, n));
 		i++;
 	}
-
 	i = 0;
-
 	typedef int (*Ft_2)(int);
 	Ft_2	ft_2[] = {&ft_isalpha, &ft_isdigit, &ft_isalnum, &ft_isascii, &ft_isprint, &ft_toupper, &ft_tolower};
 	Ft_2	ft_2_sol[] = {&isalpha, &isdigit, &isalnum, &isascii, &isprint, &toupper, &tolower};
-	printf("\n\n\n\n>>>>>>>>>>>>>>>>>>>> isalpha, isdigit, isalnum, isascii, isprint, toupper, tolower <<<<<<<<<<<<<<<<<<<<<<<<\n\n\n\n");
+	line();
+	printf("\n>>>isalpha, isdigit, isalnum, isascii, isprint, toupper, tolower<<<");
+	
 	while (i < 7)
 	{
-		
 		line();
-		refresh(str1, str2,str1_bis, str2_bis, av[1], av[2]);
+		refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
 		printf("my output :       %d\nexpected output : %d", ft_2[i](n), ft_2_sol[i](n));
 		i++;
 	}
+
+	line();
+	refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
+	printf(">>>%s<<<\n", "ft_bzero");
+	printf("my output :       %s\nexpected output : %s", ft_bzero(*p1, n_p), bzero(*p1, n_p));
+
+	line();
+	refresh(p1, p2,p1_bis, p2_bis, av[1], av[2]);
+	printf(">>>%s<<<\n", "ft_bzero");
+	printf("my output :       %s\nexpected output : %s", ft_bzero(*p1, n_p), bzero(*p1, n_p));
+
+
 	return (0);
 }
--- test.sh
+++ test.sh
@@ -3,3 +3,5 @@ if [ ! -f $(dirname "$0")/libft.a ]; then
 	make clean;
 fi
 gcc -Wall -Wextra -Werror main.c -L. -lft
+echo "./a.out \"Salut j'ai beaucoup d'imagination\" \"N'est-ce pas\" 8"
+./a.out "Salut j'ai beaucoup d'imagination" "N'est-ce pas" 8
